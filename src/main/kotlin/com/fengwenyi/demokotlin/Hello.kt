e503832c6b68d8820a3950235a5088a30c267bd9
冯文议 <xfsy_2015@163.com>
2021-08-27 14:57:14 +0800

    init

---
+++ src/main/kotlin/com/fengwenyi/demokotlin/Hello.kt
@@ -0,0 +1,12 @@
+package com.fengwenyi.demokotlin
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-08-27
+ */
+class Hello {
+
+}
+fun main(array: Array<String>) {
+    println("Hello Kotlin")
+}
